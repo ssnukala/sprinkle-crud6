fc344a63ae6828b33332167d760412e2f7e61975
ssnukala <ssnukala@users.noreply.github.com>
2025-09-18 19:40:02 +0000

    Checkpoint from VS Code for coding agent session

---
+++ app/src/Controller/ApiController.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace UserFrosting\Sprinkle\CRUD6\Controller;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use UserFrosting\Sprinkle\Core\Log\DebugLoggerInterface;
+use UserFrosting\Sprinkle\Core\I18n\Translator;
+use UserFrosting\Sprinkle\Account\Authenticate\Authenticator;
+use UserFrosting\Sprinkle\Account\Authorize\AuthorizationManager;
+
+class ApiController extends Controller
+{
+    public function __construct(
+        protected AuthorizationManager $authorizer,
+        protected Authenticator $authenticator,
+        protected DebugLoggerInterface $logger,
+        protected Translator $translator
+    ) {
+        parent::__construct($authorizer, $authenticator, $logger);
+    }
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
+    {
+        $model = $this->getModel($request);
+        $schema = $this->getSchema($request);
+        $this->validateAccess($schema, 'read');
+        $this->logger->debug("CRUD6: API request for model: {$model}");
+        $responseData = [
+            'message' => $this->translator->translate('CRUD6.API.SUCCESS', ['model' => $schema['title'] ?? $model]),
+            'model' => $model,
+            'schema' => $schema
+        ];
+        $response->getBody()->write(json_encode($responseData));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+}
--- app/src/Exceptions/SchemaNotFoundException.php
+++ app/src/Exceptions/SchemaNotFoundException.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license   https://github.com/ssnukala/sprinkle-crud6/blob/master/LICENSE.md (MIT License)
  */
 
-namespace UserFrosting\Sprinkle\CRUD6\Exceptions;
+namespace UserFrosting\Sprinkle\CRUD6\Exception;
 
 use UserFrosting\Sprinkle\Core\Exceptions\NotFoundException;
 
